04625f93fbc29059a1de3680e131a1cbb15837ae
Nick <nickwu0301@gmail.com>
2022-08-09 01:37:23 +0800

    Add CORS setting

--- src/main/java/tibame/tga102/yokult/member/controller/MemberApiController.java
+++ src/main/java/tibame/tga102/yokult/member/controller/MemberApiController.java
@@ -1,6 +1,7 @@
 package tibame.tga102.yokult.member.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import tibame.tga102.yokult.member.service.MemberService;
 import tibame.tga102.yokult.member.vo.Member;
 
+@CrossOrigin
 @RestController
 @RequestMapping(path = { "/api/0.01/member" })
 public class MemberApiController {
