a4dfd784b7c4adce45db5c0de7e04a6f1bfd1bab
Nick <nickwu0301@gmail.com>
2022-08-14 14:25:59 +0800

    update staff database

--- db.sql
+++ db.sql
@@ -200,3 +200,27 @@ values
 (28, '370', '9', '22072900010'),
 (29, '200', '2', '22072900010'),
 (30, '130', '3', '22072900010');
+
+-- 員工資料
+DROP TABLE IF EXISTS `staff`;
+
+CREATE TABLE `staff` (
+  `staff_id` varchar(50) NOT NULL COMMENT '員工編號',
+  `staff_name` varchar(50) NOT NULL COMMENT '員工姓名',
+  `staff_email` varchar(320) NOT NULL COMMENT '員工信箱',
+  `staff_idnumber` varchar(50) NOT NULL COMMENT '員工身分證字號',
+  `staff_birthday` datetime NOT NULL COMMENT '員工生日',
+  `staff_phone` varchar(20) NOT NULL COMMENT '手機號碼',
+  `staff_picture` longblob COMMENT '照片',
+  `annual_leave` varchar(50)COMMENT '特休',
+  `personal_leave` varchar(50)COMMENT '事假',
+  `official_leave` varchar(50)COMMENT '基本假',
+  PRIMARY KEY (`staff_id`))
+  comment = '員工';
+
+INSERT INTO 
+  `staff` (`staff_id`, `staff_name`, `staff_email`, `staff_idnumber`, `staff_birthday`, `staff_phone`, `staff_picture`, `annual_leave`, `personal_leave`, `official_leave`)
+VALUES
+  ('tga001', '陳花花', 'flower@gmail.com', 'a223456789', '1999-01-01','0987654399','','7','5','8'),
+  ('tga002', '黃泡泡', 'pop@gmail.com', 'a223456788', '1999-01-02','0987654388','','3','3','8'),
+  ('tga003', '林毛毛', 'green@gmail.com', 'a223456787', '1999-01-03','0987654377','','4','9','8')
